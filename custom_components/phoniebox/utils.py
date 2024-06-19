0c73dc76dd84cd304cbaee7db677317fd031debe
David Kagerer <david@kagerer.co>
2024-06-19 16:20:24 +0200

    fix: add save way to parse float and int

--- custom_components/phoniebox/utils.py
+++ custom_components/phoniebox/utils.py
@@ -1,5 +1,7 @@
 """Utility functions."""
 
+from typing import Any
+
 
 def string_to_bool(value: str) -> bool:
     """Boolean string to boolean converter."""
@@ -13,3 +15,19 @@ def bool_to_string(value: bool) -> str:  # noqa: FBT001
     if value:
         return "true"
     return "false"
+
+
+def parse_float_save(val: Any, fallback: float = 0.0) -> float:
+    """Try to parse value to float or return fallback."""
+    try:
+        return float(val)
+    except ValueError:
+        return fallback
+
+
+def parse_int_save(val: Any, fallback: int = 0) -> int:
+    """Try to parse value to int or return fallback."""
+    try:
+        return int(val)
+    except ValueError:
+        return fallback
