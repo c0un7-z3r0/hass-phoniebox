3b53e0431999b43a711e461f3113ab8c0d67b925
R. <36970351+bold-gman@users.noreply.github.com>
2025-07-21 21:29:49 +0200

    Changed layout to shorten the lines
    Adjusted the code to avoid long lines

--- custom_components/phoniebox/__init__.py
+++ custom_components/phoniebox/__init__.py
@@ -34,7 +34,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     for platform in PLATFORMS:
         if entry.options.get(platform, True):
             coordinator.platforms.append(platform)
-            await hass.config_entries.async_forward_entry_setups(entry, coordinator.platforms)
+            await hass.config_entries.async_forward_entry_setups(
+              entry, coordinator.platforms
+            )
 
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
     return True
